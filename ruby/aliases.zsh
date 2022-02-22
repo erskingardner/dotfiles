5ea6bd66714769ebc7699fb46e3ba1a581df061c
Jeff Gardner <erskingardner@gmail.com>
2022-02-22 16:13:29 +0000

    Updating ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,4 +1,7 @@
-alias rs='rails s'
 alias bd='./bin/dev' # fire up the dev server
-alias rc='rails c'
+alias rc='./bin/rails c'
 alias bi='bundle install'
+alias rmig='./bin/rails db:migrate'
+alias rt='./bin/rails test'
+alias lint='rubocop --parallel && bundler-audit'
+alias tl='rt && lint'
